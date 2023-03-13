3f85792ed4bdbcc5a07de9ae85c83ca431421e5b
Tomasz Pikć <tomasz.pikc@grupablix.pl>
2023-03-13 21:24:37 +0100

    connect inputs to GND

--- ESP32/src/main.cpp
+++ ESP32/src/main.cpp
@@ -5,17 +5,17 @@
 #include <BLEUtils.h>
 
 #define SERVICE_UUID "a129eaa4-28f6-4a61-a2b7-cb150dfcb92e"
+#define CHARACTERISTIC_UUID "e5881cf2-7b51-4dd5-a1e4-3514d2fc3235"
 
 #define RELEASE_BUTTON_PIN 15
 #define PRESS_BUTTON_PIN 4
 
-InputDebouncer releaseButton(RELEASE_BUTTON_PIN, 50, Falling);
-InputDebouncer pressButton(PRESS_BUTTON_PIN, 50, Rising);
-BLECharacteristic characteristic("e5881cf2-7b51-4dd5-a1e4-3514d2fc3235");
+InputDebouncer releaseButton(RELEASE_BUTTON_PIN, 50, Rising);
+InputDebouncer pressButton(PRESS_BUTTON_PIN, 50, Falling);
+BLECharacteristic characteristic(CHARACTERISTIC_UUID);
 BLEConnection connection;
 
 void notify(unsigned long time) {
-    Serial.println("pressed");
     if (connection.isConnected()) {
         uint8_t *value = (uint8_t *)&time;
         characteristic.setValue(value, sizeof(time));
@@ -28,8 +28,8 @@ void notify(unsigned long time) {
 
 void setup() {
     Serial.begin(9600);
-    pinMode(RELEASE_BUTTON_PIN, INPUT_PULLDOWN);
-    pinMode(PRESS_BUTTON_PIN, INPUT_PULLDOWN);
+    pinMode(RELEASE_BUTTON_PIN, INPUT_PULLUP);
+    pinMode(PRESS_BUTTON_PIN, INPUT_PULLUP);
     pressButton.setCallback(notify);
     releaseButton.setCallback(notify);
     BLEDevice::init("StopwatchBLE");
